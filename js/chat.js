aa4bc2947ab0cc1ee72a68d5c87749ddc69565a9
rshimoda <rshimoda@gmail.com>
2018-01-27 18:12:19 -0500

    getting the list of things

---
+++ js/chat.js
@@ -0,0 +1,94 @@
+
+const npcNumber = 42;
+var npcs = [
+    "3dgy3ddie", 
+    "SugarLana", 
+    "MonsterDave", 
+    "MisterMister",
+    "SweetieKatie", 
+    "Loverboy02", 
+    "ArcadeTyphoon", 
+    "HoneyLizzie", 
+    "EvilTwin", 
+    "CaptainAwesome",
+    "MartinMartini", 
+    "SinnerAndLovingIt", 
+    "OptimusPr1me", 
+    "TheJok3r",
+    "2hot4u",
+    "ModernCasanova", 
+    "NakedCowboy", 
+    "DragonTamer", 
+    "PhoenixMoons", 
+    "MySp00nIs2Big",
+    "BlueSkype", 
+    "CtrlAltDelicious", 
+    "Broddicus", 
+    "ThaRealness", 
+    "Breadquanda", 
+    "LittleMermaid", 
+    "SleepingBeauty", 
+    "SnowBlack", 
+    "Sailormoon", 
+    "PizzaPizza",
+    "BarbieGirl", 
+    "ModestMouse", 
+    "Ultraviolet", 
+    "DancingNancy", 
+    "QueenKong",
+    "DontBother", 
+    "ToastOfTheUndead", 
+    "BrosefStalin", 
+    "DrinkingTrotsky",
+    "Porkchop", 
+    "WillyFoo", 
+    "Poutine4eva", 
+    "BadKarma" ];
+
+var npcIcons = [
+    "500px"
+    ,"adn"
+    ,"amazon"
+    ,"android"
+    ,"angellist"
+    ,"apple"
+    ,"bandcamp"
+    ,"behance"
+    ,"bitbucket"
+    ,"bitcoin"
+    ,"black-tie"
+    ,"bluetooth"
+    ,"btc"
+    ,"buysellads"
+    ,"cc-amex"
+    ,"cc-diners-club"
+    ,"cc-discover"
+    ,"cc-jcb"
+    ,"cc-mastercard"
+    ,"cc-paypal"
+    ,"cc-stripe"
+    ,"cc-visa"
+    ,"chrome"
+    ,"codepen"
+    ,"codiepie"
+    ,"connectdevelop"
+    ,"contao"
+    ,"css3"
+    ,"dashcube"
+    ,"delicious"
+    ,"deviantart"
+    ,"digg"
+    ,"dribbble"
+    ,"dropbox"
+    ,"drupal"
+    ,"edge"
+    ,"eercast"
+    ,"empire"
+    ,"envira"
+    ,"etsy"
+    ,"rocket"
+    ,"ship"
+    ,"train"
+    ,"truck"
+];
+
